7af6bb78a4d1c510339128a1ed86af0b5937c6a6
Khadaassi <aassikhadija@yahoo.fr>
2024-10-25 11:50:01 +0200

    change parameters name

--- shark.py
+++ shark.py
@@ -5,8 +5,10 @@ class Shark(Fish):
     A class to represent a shark in a water ecosystem that inherites of Fish class.
     Methods
     -------
-    swim(distance: float)
-        Simulates the shark swimming a specified distance in meters, potentially affecting its health or energy.
+    swim(direction: int)
+        Simulates the shark swimming a specified direction.
+    reproduce()
+        Returns a new Shark instance if the shark is of reproductive age.
     eat(prey: 'Fish')
         Simulates the shark eating a fish.
     """
@@ -14,13 +16,13 @@ class Shark(Fish):
     def __init__(self, age: int, size: float):
         super().__init__(age, size)
 
-    def swim(self, distance: float):
+    def swim(self, direction: int):
         """
         Simulates the shark swimming a specified distance.
         
         Parameters
         ----------
-        distance : float
+        direction : int
             The distance that the shark will swim.
         """
         pass

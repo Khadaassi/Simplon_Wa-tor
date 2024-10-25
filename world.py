5d986c3e3e523260dc0da7f960e346a355cfe572
Samuel Thorez-Debrucq <S.Thorez@hotmail.fr>
2024-10-25 14:50:26 +0200

    world v.0.2

--- world.py
+++ world.py
@@ -1,16 +1,22 @@
 from random import randint
+import os
+import time
+clear = lambda: os.system("clear")
 
 
 class World:
     
-    def __init__(self, s_pop: tuple[int, int], chro_len: int, world_size: int, fish_repro_time: int, shark_repro_time: int) -> None:
+    def __init__(self, s_pop: tuple[int, int], chro_len: int, world_size: tuple[int, int], fish_repro_time: int, shark_repro_time: int,\
+        shark_energy: int, shark_energy_gain: int) -> None:
         """
         [Args]\n
         s_pop = a tuple of int containing the starting population of fishes and sharks, respectively
         chro_len = the length of a chronos for this world
-        world_size = the size of the grid (world)
+        world_size = a tuple of int defining the world dimension as follow [width, height]
         fish_repro_time = the amount of chronos a fish needs to reproduce
-        shark_repro_time = the amount of chronos a shark needs to reproduce"""
+        shark_repro_time = the amount of chronos a shark needs to reproduce
+        shark_energy = starting energy of a newly created shark
+        shark_energy_gain = amount of energy a shark gains when eating a fish"""
         
         self.starting_population = s_pop #The initial ratio of fish to shark. starting_population[0] = fishes, starting_population[1] = sharks
         self.chronos_length = chro_len #The amount of iterations of the loop necessary before the next chronos start
@@ -20,9 +26,11 @@ class World:
         self.grid = [] #The actual world. A list of lists holding, fish, sharks or FALSE(water) to simulate a grid view.
         self.current_fish_population = self.starting_population[0] #The current amount of fishes in the world
         self.current_shark_population = self.starting_population[1] #The current amount of sharks in the world
+        self.shark_energy = shark_energy #The starting energy of a shark.  
+        self.shark_energy_gained_by_eating = shark_energy_gain #The amount of energy a shark gains when eating a fish
         
         #Cannot create a world smaller than total starting population
-        if (sum(self.starting_population)) > self.size*self.size:
+        if (sum(self.starting_population)) > self.size[0]*self.size[1]:
             raise ValueError("Total population too big : try increasing world size or reducing populations")
         
         
@@ -39,9 +47,9 @@ class World:
         sharks = self.starting_population[1]
         
         #Fill the grid with water
-        for x in range(0, self.size):
+        for x in range(0, self.size[1]):
             self.grid.append([])
-            for y in range(0, self.size):
+            for y in range(0, self.size[0]):
                 self.grid[x].append("~")            
         
         #Place the fishes randomly
@@ -56,6 +64,31 @@ class World:
             self.grid[x][y] = "X"            
             sharks -= 1  
         
+    def update_world(self) -> None:
+        #Move everything
+        for x in range(0, len(self.grid)):
+            for y in range(0, len(self.grid[x])):
+                direction = self.get_direction(x, y)
+                if direction == "":
+                    continue
+                if direction == "N":
+                    self.grid[x-1][y] = self.grid[x][y]
+                    self.grid[x][y] = "~"
+                if direction == "S":
+                    self.grid[x+1][y] = self.grid[x][y]
+                    self.grid[x][y] = "~"
+                if direction == "W":
+                    self.grid[x][y-1] = self.grid[x][y]
+                    self.grid[x][y] = "~"
+                if direction == "E":
+                    self.grid[x][y+1] = self.grid[x][y]
+                    self.grid[x][y] = "~"
+        
+        self.print_grid()
+        
+        
+                
+                 
         
     def get_empty_grid_space(self) -> int:
         """
@@ -65,12 +98,13 @@ class World:
         #Loop through random coordinates until one water tile is found
         found_suitable_space = False
         while not found_suitable_space:
-            x = randint(0, self.size-1)
-            y = randint(0, self.size-1)
+            x = randint(0, self.size[1]-1)
+            y = randint(0, self.size[0]-1)
             if self.grid[x][y] == "~":
                 found_suitable_space = True
         return x, y
 
+    
 
     def print_grid(self) -> None: 
         """
@@ -82,10 +116,48 @@ class World:
                 line += f"[{y}]"
             line += " |"
             print(line)
-                
+    
+    
+    def get_direction(self, x: int, y: int) -> str:
+        outcomes = ""
+        fish = True
+        
+        if fish:
+            #TODO: gérer les bords
+            #Check for North availlable
+            if x == 0 or x == len(self.grid)-1 or y == len(self.grid[0])-1 or y == 0:
+                return ""
+            if self.grid[x-1][y] == "~":
+                outcomes += "N"
+            #Check for South availlable
+            if self.grid[x+1][y] == "~":
+                outcomes += "S"
+            #Check for West availlable
+            if self.grid[x][y-1] == "~":
+                outcomes += "W"
+            #check for East availlabke
+            if self.grid[x][y+1] == "~":
+                outcomes += "E"
+        else:
+            pass
+        
+        return outcomes
+                     
         
     
     
-my_world = World((50, 70), 8, 25, 10, 10)
+my_world = World((2, 0), 5, (5, 5), 10, 10, 5, 5)
 my_world.populate_world()
-my_world.print_grid()
+max_loop = 10
+current_loop = 0
+compteur = 0
+start_t = time.time()
+while current_loop < max_loop: 
+    if time.time() - start_t >= my_world.chronos_length:
+        
+        start_t = time.time()
+        current_loop += 1
+        clear()
+        print("current loop : ", current_loop)
+        my_world.update_world()
+    

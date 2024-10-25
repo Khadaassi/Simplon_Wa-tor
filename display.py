24d81a8e5a81d1d87c0b23663d6c76d93275b6f3
NicolasCassonnet <nicolas.cassonnet@wanadoo.fr>
2024-10-25 15:17:58 +0200

    add just the minimum doctstrings

--- display.py
+++ display.py
@@ -1,6 +1,26 @@
 class display:
-    def __init__(self, main_object):
-        self.mainObject = main_object
+    """
+        Management of the pygame interface
+    """
+    def __init__(self, app_object):
+        """
+            When instanciated, the object starts a pygame
+            needs a app_object which have got three methods : 
+                app_object.start()
+                app_object.pause()
+                app_object.stop()
+        """
+        self.mainObject = app_object
 
     def update_view(self, world):
+        """
+            Takes a world object which
+              is a list[list[str]] object 
+              or contains a list[list[str]]
+            in which  ashark is a 'X', a fish is a 'o', water is a '~'
+        """
         self.world = world
+
+# app_object.start()
+# app_object.pause()
+# app_object.stop()

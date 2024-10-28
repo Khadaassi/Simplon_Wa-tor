60bed51f49d35bd71e093f481d7e86c3450699e4
Khadaassi <aassikhadija@yahoo.fr>
2024-10-28 09:21:39 +0100

    adding shark class

--- fish.py
+++ fish.py
@@ -35,3 +35,34 @@ class Fish():
             return True
         else:
             return False
+
+
+class Shark(Fish):
+    """"
+    A class to represent a shark in a water ecosystem that inherites of Fish class.
+    Methods
+    -------
+    reproduce()
+        Returns a new Shark instance if the shark is of reproductive age.
+    eat()
+        Simulates the shark eating a fish.
+    """
+
+    def __init__(self, reproduction_age: int):
+        super().__init__(reproduction_age)
+        pass
+
+
+    def reproduce(self):
+        """
+        Returns a new Shark instance if the shark is of reproductive age.
+        """
+        pass
+
+    def eat(self):
+        """
+        Simulates the shark eating a fish.
+        
+        """
+        pass
+#energie shark 

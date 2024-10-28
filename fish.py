4656f7d6f5500f30a067dc403b7e5d23110e2856
Khadaassi <aassikhadija@yahoo.fr>
2024-10-28 09:40:00 +0100

    add energy and eat method for shark class, refactor docstrings

--- fish.py
+++ fish.py
@@ -5,19 +5,24 @@ class Fish():
     Attributes
     ----------
     age : int
-        The age of the fish in years.
+        The age of the fish.
+    reproduction_age : int
+        The age at which the fish can reproduce.
+    direction : str
+        The direction in which the fish is moving.
+    has_moved : bool
+        A boolean indicating if the fish has moved during the current loop.
 
     Methods
-    --------  
+    -------
     reproduce()
         Returns a new Fish instance if the fish is of reproductive age.
-
     """
 
     def __init__(self, reproduction_age: int, has_moved: bool):
         self.age = 0
         self.reproduction_age = reproduction_age
-        self.direction = ""
+        self.direction = "" # for later
         self.has_moved = has_moved
 
     def reproduce(self):
@@ -40,29 +45,45 @@ class Fish():
 class Shark(Fish):
     """"
     A class to represent a shark in a water ecosystem that inherites of Fish class.
+    
+    Attributes
+    ----------
+    energy : int
+        The energy of the shark.
+    max_energy : int
+        The maximum energy of the shark.
+    reproduction_age : int
+        The age at which the shark can reproduce.
+    
     Methods
     -------
-    reproduce()
-        Returns a new Shark instance if the shark is of reproductive age.
-    eat()
+    eat(energy: int)
         Simulates the shark eating a fish.
+    energy_management(energy: int)
+        Simulates the shark energy movement.
     """
 
-    def __init__(self, reproduction_age: int):
+    def __init__(self, reproduction_age: int, energy: int):
         super().__init__(reproduction_age)
+        self.energy = energy
+        self.max_energy = self.energy
         pass
 
 
-    def reproduce(self):
-        """
-        Returns a new Shark instance if the shark is of reproductive age.
-        """
-        pass
-
-    def eat(self):
+    def eat(self, energy: int):
         """
         Simulates the shark eating a fish.
-        
         """
-        pass
-#energie shark 
+        self.energy += energy
+        if self.energy > self.max_energy:
+            self.energy = self.max_energy
+
+    def energy_management(self, energy: int):
+        """
+        Simulates the shark energy movement.
+        """
+        self.energy -= energy
+        if self.energy == 0:
+            return False
+        else:
+            return True

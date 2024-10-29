94b7a45317f453c8d6b45021ab562b1e34f6106b
Khadaassi <aassikhadija@yahoo.fr>
2024-10-29 09:39:20 +0100

    change format of files

--- UserButton.py
+++ UserButton.py
@@ -1,15 +1,24 @@
 import pygame
 
-class UserButton :
 
-    def __init__(self, name:str, callback_function, x_position:int, y_position:int, x_width:int, y_height:int):
+class UserButton:
+
+    def __init__(
+        self,
+        name: str,
+        callback_function,
+        x_position: int,
+        y_position: int,
+        x_width: int,
+        y_height: int,
+    ):
         self.name = name
         self.callback_function = callback_function
         self.x_position = x_position
         self.y_position = y_position
         self.x_width = x_width
         self.y_height = y_height
-        #______________________________________________________________________
+        # ______________________________________________________________________
         # Create a font object
         font = pygame.font.Font(None, 24)
 
@@ -20,12 +29,16 @@ class UserButton :
         self.text_render = font.render(self.name, True, (0, 0, 0))
         self.text_rect = self.text_render.get_rect(
             center=(
-                self.button_surface.get_width()/2, 
-                self.button_surface.get_height()/2))
+                self.button_surface.get_width() / 2,
+                self.button_surface.get_height() / 2,
+            )
+        )
 
         # Create a pygame.Rect object that represents the button's boundaries
-        self.button_rect = pygame.Rect(self.x_position, self.y_position, self.x_width, self.y_height)  # Adjust the position as needed
-        #______________________________________________________________________
+        self.button_rect = pygame.Rect(
+            self.x_position, self.y_position, self.x_width, self.y_height
+        )  # Adjust the position as needed
+        # ______________________________________________________________________
 
     def check_event(self, event):
         # Check for the mouse button down event
@@ -33,37 +46,37 @@ class UserButton :
             # Call the on_mouse_button_down() function
             if self.button_rect.collidepoint(event.pos):
                 self.callback_function(self.name)
- 
+
     def show(self, screen: pygame.Surface):
 
         # Check if the mouse is over the button. This will create the button hover effect
         if self.button_rect.collidepoint(pygame.mouse.get_pos()):
             pygame.draw.rect(
-                self.button_surface, 
-                (127, 255, 212), 
-                (1, 1, self.x_width-2, self.y_height-2))
+                self.button_surface,
+                (127, 255, 212),
+                (1, 1, self.x_width - 2, self.y_height - 2),
+            )
         else:
             pygame.draw.rect(
-                self.button_surface, 
-                (0, 0, 0), 
-                (0, 0, self.x_width, self.y_height))
-            pygame.draw.rect(
-                self.button_surface, 
-                (255, 255, 255), 
-                (1, 1, self.x_width-2, self.y_height-2))
+                self.button_surface, (0, 0, 0), (0, 0, self.x_width, self.y_height)
+            )
             pygame.draw.rect(
                 self.button_surface,
-                (0, 0, 0), 
-                (1, 1, self.x_width-2, 1), 
-                2)
+                (255, 255, 255),
+                (1, 1, self.x_width - 2, self.y_height - 2),
+            )
             pygame.draw.rect(
-                self.button_surface, 
-                (0, 100, 0), 
-                (1, self.y_height-2, self.x_width-2, 10), 
-                2)
-        #__________________________________________________________________________
-        
-        #__________________________________________________________________________
+                self.button_surface, (0, 0, 0), (1, 1, self.x_width - 2, 1), 2
+            )
+            pygame.draw.rect(
+                self.button_surface,
+                (0, 100, 0),
+                (1, self.y_height - 2, self.x_width - 2, 10),
+                2,
+            )
+        # __________________________________________________________________________
+
+        # __________________________________________________________________________
         # Show the button text
         self.button_surface.blit(self.text_render, self.text_rect)
 
--- main.py
+++ main.py
@@ -1,60 +1,60 @@
 import os
 import time
-from plot import plot_population 
+from plot import plot_population
 from fish import Shark
 from world import World
 from WaTorDisplay import *
 
 
-clear = lambda: os.system('cls' if os.name == 'nt' else 'clear')
+clear = lambda: os.system("cls" if os.name == "nt" else "clear")
+
 
 def main():
     """
     Main function to run the simulation
     """
 
-
-    world = World((500, 200), 0.001, (40,30), 3, 3, 2, 2)
+    world = World((500, 200), 0.001, (40, 30), 3, 3, 2, 2)
     world.populate_world()
     current_iteration = 0
-    start_t = time.time() # Start time
-    fish_population = [] # List to store fish population
-    shark_population = [] # List to store shark population
-    iterations = [] # List to store iterations
+    start_t = time.time()  # Start time
+    fish_population = []  # List to store fish population
+    shark_population = []  # List to store shark population
+    iterations = []  # List to store iterations
     counter = 0
     print_counter = 0
 
-    
     print("Initial World State:")
     world.print_grid()
 
+    display = WaTorDisplay()  # initialize View
+    display.update_view(world)  # create screen with the first world
 
-    display = WaTorDisplay() # initialize View
-    display.update_view(world) # create screen with the first world
-    
-    time.sleep(1) # Sleep for 1 second
+    time.sleep(1)  # Sleep for 1 second
     clear()
 
-    while True: 
-        if display.state == DisplayState.STOP : 
+    while True:
+        if display.state == DisplayState.STOP:
             break
 
-        if display.state != DisplayState.PLAY or display.state == DisplayState.PAUSE: 
+        if display.state != DisplayState.PLAY or display.state == DisplayState.PAUSE:
             display.update_view(world)
             continue
 
-        if time.time() - start_t >= world.chronos_length: 
+        if time.time() - start_t >= world.chronos_length:
             counter += 1
-            print_counter += 1       
+            print_counter += 1
             start_t = time.time()
             current_iteration += 1
 
             clear()
             print("Current iteration : ", current_iteration)
             world.update_world()
-            display.update_view(world) # update screen with the next world
+            display.update_view(world)  # update screen with the next world
             world.print_grid()
-            print(f"Fish pop : {world.fish_population} ; Shark pop : {world.shark_population}")
+            print(
+                f"Fish pop : {world.fish_population} ; Shark pop : {world.shark_population}"
+            )
             fish_population.append(world.fish_population)
             shark_population.append(world.shark_population)
             iterations.append(current_iteration)
@@ -63,9 +63,10 @@ def main():
 
             break
 
-    plot_population(iterations, fish_population, shark_population)    
-    while display.state != DisplayState.OUT : display.update_view(world)
- 
+    plot_population(iterations, fish_population, shark_population)
+    while display.state != DisplayState.OUT:
+        display.update_view(world)
+
 
 if __name__ == "__main__":
     main()

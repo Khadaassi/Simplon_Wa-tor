59521b9e0137e030119b7b90346c11d9ab928578
Khadaassi <aassikhadija@yahoo.fr>
2024-10-29 15:19:34 +0100

    create and read configfile and integration in main

--- main.py
+++ main.py
@@ -4,22 +4,30 @@ from plot import plot_population
 from fish import Shark
 from world import World
 from WaTorDisplay import *
+from configfile import *
 
 
 clear = lambda: os.system("cls" if os.name == "nt" else "clear")
 
+
+if not os.path.exists("config.ini"):
+    write_config()
+
+config = read_config()
 
 def main():
     """
     Main function to run the simulation
     """
 
-    world = World((500, 200), 0.001, (40, 30), 3, 3, 2, 2)
+    world = World((config[0], config[1]), config[2], (config[3], config[4]), config[5], config[6], config[7], config[8], config[9], config[10])
+    
     world.populate_world()
     current_iteration = 0
     start_t = time.time()  # Start time
     fish_population = []  # List to store fish population
     shark_population = []  # List to store shark population
+    megalodon_population = []  # List to store megalodon population
     iterations = []  # List to store iterations
     counter = 0
     print_counter = 0
@@ -53,17 +61,18 @@ def main():
             display.update_view(world)  # update screen with the next world
             world.print_grid()
             print(
-                f"Fish pop : {world.fish_population} ; Shark pop : {world.shark_population}"
+                f"Fish pop : {world.fish_population} ; Shark pop : {world.shark_population} ; Megalodon pop : {world.megalodon_population}"
             )
             fish_population.append(world.fish_population)
             shark_population.append(world.shark_population)
+            megalodon_population.append(world.megalodon_population)
             iterations.append(current_iteration)
 
         if world.fish_population == 0 or world.shark_population == 0 or counter == 100:
 
             break
 
-    plot_population(iterations, fish_population, shark_population)
+    plot_population(iterations, fish_population, shark_population, megalodon_population)
     while display.state != DisplayState.OUT:
         display.update_view(world)
 
--- plot.py
+++ plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-def plot_population(iterations, fish_population, shark_population):
+def plot_population(iterations, fish_population, shark_population, megalodon_population):
 
     plt.figure(figsize=(12, 7))
 
@@ -8,6 +8,8 @@ def plot_population(iterations, fish_population, shark_population):
     plt.plot(iterations, fish_population, color="royalblue", linewidth=2.5, label="Population de Poissons")
 
     plt.plot(iterations, shark_population, color="darkorange", linewidth=2.5, label="Population de Requins")
+
+    plt.plot(iterations, megalodon_population, color="purple", linewidth=2.5, label="Population de Mégalodons")
 
 
     plt.title("Évolution de la Population de Poissons et de Requins au Fil du Temps", fontsize=16, fontweight="bold")
